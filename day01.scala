95ae5025f0ee44d42c9708741cb8ad8b9c30855c
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2021-12-12 15:41:55 +0000

    Day 1 part 1

---
+++ day01.scala
@@ -0,0 +1,24 @@
+object day01 {
+
+  def readLines(filename: String): List[String] = {
+    val source = io.Source.fromFile(filename)
+    try
+      source.getLines().toList
+    finally
+      source.close()
+  }
+
+  def checkAnswer(day: Int, part: Int, answer: Long) = {
+    println(answer)
+    val dayString = "%02d".format(day)
+    val lines = readLines(s"day_${dayString}_answers.txt")
+    assert(lines(part-1).toLong == answer)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val depths = readLines("day_01_input.txt").map(_.toInt)
+    def isDrop(vals: List[Int]) = vals(0) < vals(1)
+    val drops = depths.sliding(2).count(isDrop _)
+    checkAnswer(1, 1, drops)
+  }
+}
