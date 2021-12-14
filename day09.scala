f8d7091cd0e03edcabab5d4fd2c63e3f873fe159
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2021-12-14 19:34:42 +0000

    Day 9 part 2

--- day09.scala
+++ day09.scala
@@ -2,7 +2,7 @@ object day09 {
 
   import day01.{readLines, checkAnswer}
 
-  class Point(val row: Int, val col: Int)(implicit heightMap: Seq[Seq[Int]]) {
+  sealed case class Point(row: Int, col: Int)(implicit heightMap: Seq[Seq[Int]]) {
     lazy val height: Int = heightMap(row)(col)
 
     def neighbours = List(
@@ -13,6 +13,14 @@ object day09 {
     ).flatten
 
     def isLow: Boolean = !neighbours.map(_.height <= height).contains(true)
+
+    @annotation.tailrec
+    def floodFill(start: Set[Point] = Set(this)): Set[Point] = {
+      val expanded = (start union start.flatMap(_.neighbours)).filter(_.height != 9)
+      if (expanded.size == start.size) start else floodFill(expanded)
+    }
+
+    def basinSize: Int = floodFill().size
   }
 
   object Point {
@@ -30,5 +38,11 @@ object day09 {
     val lowpoints = Point.all.filter(_.isLow)
     val total = lowpoints.map(_.height + 1).sum
     checkAnswer(9, 1, total)
+
+    // Basins seem a bit under-specified, since it doesn't say which basin the
+    // 8 belongs to in this layout: 080. Let's proceed as if every low point
+    // is separated by 9s, and hope the answer comes out right.
+    val basins = lowpoints.map(_.basinSize).toSeq.sorted.takeRight(3).product
+    checkAnswer(9, 2, basins)
   }
 }
