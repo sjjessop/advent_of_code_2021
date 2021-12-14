14a360122f14475591111706485f62c53e09169b
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2021-12-14 19:33:07 +0000

    Refactor day 9

--- day09.scala
+++ day09.scala
@@ -2,23 +2,33 @@ object day09 {
 
   import day01.{readLines, checkAnswer}
 
-  def isLowPoint(rowIdx: Int, colIdx: Int, height: Seq[Seq[Int]]): Boolean = {
-    val self = height(rowIdx)(colIdx)
-    if (rowIdx > 0 && height(rowIdx - 1)(colIdx) <= self) false
-    else if (colIdx > 0 && height(rowIdx)(colIdx - 1) <= self) false
-    else if (rowIdx < height.length - 1 && height(rowIdx + 1)(colIdx) <= self) false
-    else if (colIdx < height(0).length - 1 && height(rowIdx)(colIdx + 1) <= self) false
-    else true
+  class Point(val row: Int, val col: Int)(implicit heightMap: Seq[Seq[Int]]) {
+    lazy val height: Int = heightMap(row)(col)
+
+    def neighbours = List(
+      Option.when(row > 0)(Point(row - 1, col)),
+      Option.when(col > 0)(Point(row, col - 1)),
+      Option.when(row < heightMap.length - 1)(Point(row + 1, col)),
+      Option.when(col < heightMap(0).length - 1)(Point(row, col + 1)),
+    ).flatten
+
+    def isLow: Boolean = !neighbours.map(_.height <= height).contains(true)
+  }
+
+  object Point {
+    def apply(row: Int, col: Int)(implicit heightMap: Seq[Seq[Int]]): Point = new Point(row, col)
+    def all(implicit heightMap: Seq[Seq[Int]]): Iterable[Point] = (
+      for (row <- 0 until heightMap.length; col <- 0 until heightMap(0).length)
+      yield Point(row, col)
+    )
   }
 
   def main(args: Array[String]): Unit = {
-    val heightMap = readLines("day_09_input.txt").toSeq.map(r => r.toSeq.map(_.toString.toInt))
+    implicit val heightMap = readLines("day_09_input.txt").toSeq.map(r => r.toSeq.map(_.toString.toInt))
     val widths = heightMap.map(_.length)
-    val width = widths.max
-    assert(width == widths.min)
-    val scores = for (rowIdx <- 0 until heightMap.length; colIdx <- 0 until width)
-      yield (if (isLowPoint(rowIdx, colIdx, heightMap)) heightMap(rowIdx)(colIdx) + 1 else 0)
-    val total = scores.sum
+    assert(widths.max == widths.min)
+    val lowpoints = Point.all.filter(_.isLow)
+    val total = lowpoints.map(_.height + 1).sum
     checkAnswer(9, 1, total)
   }
 }
