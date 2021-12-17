192952bfd8f7b427b623b5492c0bfaca25c52a8d
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2021-12-17 15:29:42 +0000

    Day 13 part 2

--- day13.scala
+++ day13.scala
@@ -3,17 +3,37 @@ object day13 {
   import day01.{readLines, checkAnswer}
   import day05.Pos
 
-  case class Manual(points: Set[Pos], folds: List[String]) {
+  sealed case class Manual(points: Set[Pos], folds: List[String]) {
     def foldX(idx: Int): Set[Pos] = {
       points.filter(_.x < idx).union(
         points.filter(_.x > idx).map{case Pos(x, y) => Pos(2 * idx - x, y)}
+      )
+    }
+    def foldY(idx: Int): Set[Pos] = {
+      points.filter(_.y < idx).union(
+        points.filter(_.y > idx).map{case Pos(x, y) => Pos(x, 2 * idx - y)}
       )
     }
     def foldNext: Manual = {
       val instruction = folds.head
       if (instruction("fold along ".size) == 'x')
         Manual(foldX(instruction.substring("fold along x=".size).toInt), folds.tail)
-      else ???
+      else
+        Manual(foldY(instruction.substring("fold along x=".size).toInt), folds.tail)
+    }
+    @annotation.tailrec
+    def foldAll: Manual = folds match {
+      case Nil => this
+      case _ => this.foldNext.foldAll
+    }
+    def vis(): Seq[String] = {
+      val width = points.map(_.x).max
+      val height = points.map(_.y).max
+      (0 to height).map(row =>
+        (0 to width)
+        .map(x => if (points contains Pos(x, row)) '*' else ' ')
+        .mkString
+      )
     }
   }
 
@@ -32,5 +52,10 @@ object day13 {
     val manual = Manual.parse(readLines("day_13_input.txt"))
     val firstFoldShowing = manual.foldNext.points.size
     checkAnswer(13, 1, firstFoldShowing)
+
+    val code = manual.foldAll.vis()
+    println(code.mkString("\n"))
+    def chomp(s: String) = s.reverse.dropWhile(_ == ' ').reverse
+    assert(code.map(chomp) == readLines("day_13_answers.txt").drop(1).map(chomp))
   }
 }
