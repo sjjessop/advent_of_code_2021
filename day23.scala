03e30d0df30d1abaae38b857cc4ce92c4acb8f28
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2021-12-24 10:25:59 +0000

    Day 23 part 2

--- day23.scala
+++ day23.scala
@@ -90,27 +90,27 @@ object day23 {
   // don't need to store them in the packed representation, just in the hallway
   // expansion. Would save 4 chars per graph node. We could even pack down to
   // 2 bits per space, thus saving the whole thing in 30 bits of an Int.
-
-  def atrium(pos: Int): Int = pos match {
-    case 0 | 1 => 10
-    case 2 | 3 => 12
-    case 4 | 5 => 14
-    case 6 | 7 => 16
-    case _ => throw new Exception("bad")
-  }
-
-  def distance(src: Int, dest: Int): Int = {
-    if (src >= 8 && dest >= 8) (src - dest).abs
-    else if (src < 8) distance(atrium(src), dest) + (if (src % 2 == 0) 2 else 1)
-    else /* (dest < 8) */ distance(atrium(dest), src) + (if (dest % 2 == 0) 2 else 1)
-  }
+  //
+  // Part 2 will be similar, but 4 positions per room.
 
   // Helper object to cache data and whatever. We'll dispose of it once we
   // know the exits from this board position, since we never need it again.
-  case class Board(packed: String) {
-    assert(packed.size >= 8)
-    assert(packed.size <= 19)
-    val hallway = (packed + "           ").substring(8)
+  case class Board(packed: String, roomSize: Int = 2) {
+    val hallwayOffset = 4 * roomSize
+    assert(packed.size >= hallwayOffset)
+    assert(packed.size <= hallwayOffset + 11)
+    val hallway = (packed + "           ").substring(hallwayOffset)
+
+    // The atrium (or doorway) to a room is the space in the hall just outside.
+    def atrium(pos: Int): Int =
+      if (pos < hallwayOffset) hallwayOffset + 2 + 2 * (pos / roomSize)
+      else ???
+
+    def distance(src: Int, dest: Int): Int = {
+      if (src >= hallwayOffset && dest >= hallwayOffset) (src - dest).abs
+      else if (src < hallwayOffset) distance(atrium(src), dest) + roomSize - (src % roomSize)
+      else distance(atrium(dest), src) + roomSize - (dest % roomSize)
+    }
 
     def exits: Iterable[(String, Int)] =
       pieceCanFinish match {
@@ -125,20 +125,17 @@ object day23 {
     def roomCanFinish(ch: Char): Option[(String, Int)] =
       for (dest <- roomOpen(ch); src <- findFinisher(ch)) yield move(src, dest)
 
-    def roomLowerIdx(ch: Char) = (ch - 'A') * 2
-    def roomUpperIdx(ch: Char) = (ch - 'A') * 2 + 1
-    def roomLower(ch: Char) = packed(roomLowerIdx(ch))
-    def roomUpper(ch: Char) = packed(roomUpperIdx(ch))
+    def roomLowerIdx(ch: Char) = (ch - 'A') * roomSize
 
     // The position (if any) in a room where the next piece that belongs in
     // that room should move to.
     def roomOpen(ch: Char): Option[Int] = {
-      if (roomUpper(ch) != ' ') None
-      else roomLower(ch) match {
-        case ' ' => Some(roomLowerIdx(ch))
-        case `ch` => Some(roomUpperIdx(ch))
-        case _ => None
-      }
+      // A room is open if and only if it consists of 0 or more of the correct
+      // piece followed one or more spaces
+      val roomStart = roomLowerIdx(ch)
+      val roomContent = packed.substring(roomStart, roomStart + roomSize)
+      val roomSpace = roomContent.dropWhile(_ == ch)
+      Option.when(roomSpace.toSet == Set(' '))(roomStart + roomSize - roomSpace.size)
     }
 
     def isClearLeft(where: Int, door: Int): Boolean =
@@ -161,21 +158,26 @@ object day23 {
         (fromLeft ++ fromRight)
           .filter(src => hallway(src) == ch)
           .filter(src => isHallwayClear(src, target))
-          .map(_ + 8)
+          .map(_ + hallwayOffset)
       }
 
       // Moves from another room into this room.
       val fromRoom = {
-        val thisRoom = Set(roomLowerIdx(ch), roomUpperIdx(ch))
+        val roomStart = roomLowerIdx(ch)
+        val thisRoom = (roomStart until roomStart + roomSize)
+        def endOf(idx: Int) = idx + (roomSize - idx % roomSize)
         // Positions in a room (not the hallway)...
-        packed.view.take(8).zipWithIndex
+        packed.view.take(hallwayOffset).zipWithIndex
           // ...containing a piece of the correct type
           .filter(_._1 == ch)
           .map(_._2)
           // ...in a different room, not this room.
           .filter(src => !thisRoom.contains(src))
           // ...with a clear road from this doorway to the target doorway
-          .filter(src => isHallwayClear(atrium(src) - 8, target))
+          // (we've already established the road from the target doorway in roomOpen)
+          .filter(src => isHallwayClear(atrium(src) - hallwayOffset, target))
+          // ...and a clear road from the piece to this doorway
+          .filter(src => ((src + 1) until endOf(src)).map(packed).filter(_ != ' ').isEmpty)
       }
 
       (fromHallway ++ fromRoom).headOption
@@ -198,23 +200,25 @@ object day23 {
       // Can't say what order we should do the moves to hallway, since any
       // move makes some other move impossible. So, check them all.
       def movable(room: Char): List[Int] = {
-        val upperIdx = roomUpperIdx(room)
-        val lowerIdx = roomLowerIdx(room)
-        val upperChar = packed(upperIdx)
-        val lowerChar = packed(lowerIdx)
-        if (upperChar != ' ' && upperChar != room) List(upperIdx)
-        else if (upperChar != ' ' && lowerChar != room) List(upperIdx)
-        else if (upperChar == ' ' && lowerChar != ' ' && lowerChar != room) List(lowerIdx)
+        // A room is movable if the top piece in it doesn't belong there,
+        // or if a piece behind that needs to get out past it.
+        val roomStart = roomLowerIdx(room)
+        val roomContent = packed.substring(roomStart, roomStart + roomSize).trim()
+        val roomEnd = roomStart + roomContent.size - 1
+        if (roomContent.size == 0) List()
+        else if (roomContent.last != room) List(roomEnd)
+        else if (roomContent.dropWhile(_ != room) != roomContent) List(roomEnd)
         else List()
       }
       for (
         room <- roomNames;
         src <- movable(room);
         dest <- List(0, 1, 3, 5, 7, 9, 10).filter(dest => hallway(dest) == ' ').filter(dest => {
-          val roomAtrium = atrium(roomLowerIdx(room)) - 8
+          val roomAtrium = atrium(roomLowerIdx(room)) - hallwayOffset
+          // println(s"checking out room=${room} src=${src} dest=${dest} door=${roomAtrium} ok=${isHallwayClear(dest, roomAtrium)}")
           isHallwayClear(dest, roomAtrium)
         })
-      ) yield move(src, dest + 8)
+      ) yield move(src, dest + hallwayOffset)
     }
   }
 
@@ -233,14 +237,36 @@ object day23 {
     println(s"  ${x(0)}#${x(2)}#${x(4)}#${x(6)}")
   }
 
+  def prettyPrint4(in: String): Unit = {
+    val x = in.map(ch => if (ch == ' ') '.' else ch)
+    println(x)
+    println(x.substring(16))
+    println(s"  ${x(3)}#${x(7)}#${x(11)}#${x(15)}")
+    println(s"  ${x(2)}#${x(6)}#${x(10)}#${x(14)}")
+    println(s"  ${x(1)}#${x(5)}#${x(9)}#${x(13)}")
+    println(s"  ${x(0)}#${x(4)}#${x(8)}#${x(12)}")
+  }
+
   def doTest(start: String, next: String): Unit = {
     val nextMoves = Board(start).exits.map(_._1).toList
+    assert(nextMoves.contains(next))
+  }
+
+  def doTest4(start: String, next: String): Unit = {
+    val nextMoves = Board(start, 4).exits.map(_._1).toList
     assert(nextMoves.contains(next))
   }
 
   def test(): Unit = {
     def grow(value: String) = List(value + "a" -> 1, value + "aaaa" -> 2)
     assert(dijkstra("", "aaaaaaaaaaaa")(grow)._1 == 6) // not 12
+
+    assert(Board("AABBCCDD").atrium(0) == 10)
+    assert(Board("AABBCCDD").distance(0, 10) == 2)
+    assert(Board("AABBCCDD").distance(8, 1) == 3)
+    assert(Board("AAAABBBBCCCCDDDD", 4).atrium(0) == 18)
+    assert(Board("AAAABBBBCCCCDDDD", 4).distance(0, 18) == 4)
+    assert(Board("AAAABBBBCCCCDDDD", 4).distance(16, 1) == 5)
 
     // Stupidly easy to solve
     assert(solve("A BBCCDDA") == 3)
@@ -271,10 +297,38 @@ object day23 {
     // Regression test for bug where spaces were trimmed off the front
     val nextMoves = getExits("D CABCB D  A")
     assert(nextMoves.count(_._1 == "CABCB DD A") == 0)
+
+    doTest4("ADDBDBCCCABBACAD", "ADDBDBCCCABBACA           D")
+    doTest4("ADDBDBCCCABBACA           D", "ADDBDBCCCABBAC  A         D")
+    doTest4("ADDBDBCCCABBAC  A         D", "ADDBDBCCCAB AC  A        BD")
+    doTest4("ADDBDBCCCAB AC  A        BD", "ADDBDBCCCA  AC  A      B BD")
+    doTest4("ADDBDBCCCA  AC  A      B BD", "ADDBDBCCC   AC  AA     B BD")
+    doTest4("ADDBDBCCC   AC  AA     B BD", "ADDBDBC CC  AC  AA     B BD")
+    doTest4("ADDBDBC CC  AC  AA     B BD", "ADDBDB  CCC AC  AA     B BD")
+    doTest4("ADDBDB  CCC AC  AA     B BD", "ADDBD   CCC AC  AA   B B BD")
+    doTest4("ADDBD   CCC AC  AA   B B BD", "ADDB    CCC AC  AA D B B BD")
+    doTest4("ADDB    CCC AC  AA D B B BD", "ADDBB   CCC AC  AA D   B BD")
+    doTest4("ADDBB   CCC AC  AA D   B BD", "ADDBBB  CCC AC  AA D     BD")
+    doTest4("ADDBBB  CCC AC  AA D     BD", "ADDBBBB CCC AC  AA D      D")
+    doTest4("ADDBBBB CCC AC  AA D      D", "ADDBBBB CCCCA   AA D      D")
+    doTest4("ADDBBBB CCCCA   AA D      D", "ADDBBBB CCCC    AA D     AD")
+    doTest4("ADDBBBB CCCC    AA D     AD", "ADDBBBB CCCCD   AA       AD")
+    doTest4("ADDBBBB CCCCD   AA       AD", "ADD BBBBCCCCD   AA       AD")
+    doTest4("ADD BBBBCCCCD   AA       AD", "AD  BBBBCCCCDD  AA       AD")
+    doTest4("AD  BBBBCCCCDD  AA       AD", "A   BBBBCCCCDDD AA       AD") // here a difference
+    doTest4("A   BBBBCCCCDDD AA       AD", "AA  BBBBCCCCDDD A        AD") // here
+    doTest4("AA  BBBBCCCCDDD A        AD", "AAA BBBBCCCCDDD          AD") // matches again
+    doTest4("AAA BBBBCCCCDDD          AD", "AAAABBBBCCCCDDD           D")
+    doTest4("AAAABBBBCCCCDDD           D", "AAAABBBBCCCCDDDD")
+    assert(solve4("ADDBDBCCCABBACAD") == 44169)
+
+    // Regression test another illegal move
+    assert(Board("A   DBCCCAB ACA B    D D DB", 4).exits.count(_._1 == "A   DBCCCAB AC  B  A D D DB") == 0)
   }
 
   def getExits(pos: String) = Board(pos).exits
   def solve(start: String) = dijkstra(start, "AABBCCDD")(getExits)._1
+  def solve4(start: String) = dijkstra(start, "AAAABBBBCCCCDDDD")(pos => Board(pos, 4).exits)._1
 
   def main(args: Array[String]): Unit = {
     test()
@@ -282,5 +336,11 @@ object day23 {
     val problem = readFile("day_23_input.txt")
     val result = solve(problem)
     checkAnswer(23, 1, result)
+
+    def unfold(x: String) = {
+      x.substring(0,1) + "DD" + x.substring(1,3) + "BC" + x.substring(3,5) + "AB" + x.substring(5,7) + "CA" + x.substring(7,8)
+    }
+    val result4 = solve4(unfold(problem))
+    checkAnswer(23, 2, result4)
   }
 }
